091ecc83d45187b2d3c010a07d09ba91984411e5
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-09-05 15:11:29 +0600

    Schema define in graphql

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,27 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from './db.js';
 
 const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
+    type Product{
+        id: ID!
+        name: String
+        image: String
+        description: String
+        price: Float
+        quantity: Int
+        onStock: Boolean
+        category: String
+    }
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
+    type Query{
+        products: [Product]
+    }
+`;
 
 const resolvers = {
   Query: {
-    books: () => books,
+    products: () => db.products,
   },
 };
 
